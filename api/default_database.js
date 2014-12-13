25ce06d30acb7807e529a496ca8b6c9767bc311c
kaliazur <kaliazur@gmail.com>
2014-12-14 01:53:37 +0800

    default values added, linked ingredients and recipes

--- api/default_database.js
+++ api/default_database.js
@@ -4,33 +4,33 @@ exports.recipes = [
         "name":"Nom de la recette 1",
         "description":"Description de la recette 1",
         "duration":"1H15",
-        "steps":"[]",
-        "ingredients":"[]",
-        "products":"[]",
+        "steps":[],
+        "ingredients":["11111", "22222"],
+        "products":[],
         "submit_date":"2014-12-12",
-        "tags":"[]"
+        "tags":[]
     },
     {
 	"_uid":"2222",
         "name":"Nom de la recette 2",
         "description":"Description de la recette 2",
         "duration":"2H10",
-        "steps":"[]",
-        "ingredients":"[]",
-        "products":"[]",
+        "steps":[],
+        "ingredients":["22222", "33333"],
+        "products":[],
         "submit_date":"2014-12-12",
-        "tags":"[]"
+        "tags":[]
     },
     {
 	"_uid":"3333",
         "name":"Nom de la recette 3",
         "description":"Description de la recette 3",
         "duration":"2H42",
-        "steps":"[]",
-        "ingredients":"[]",
-        "products":"[]",
+        "steps":[],
+        "ingredients":["11111", "33333"],
+        "products":[],
         "submit_date":"2014-12-12",
-        "tags":"[]"
+        "tags":[]
     }
 ];
 
@@ -38,18 +38,21 @@ exports.recipes = [
 
 exports.ingredients = [
     {
+	"_id":"11111",
 	"name":"eau",
         "components":"2L",
         "tags":["11", "22", "33"],
         "nutrients":[]
     },
     {
+	"_id":"22222",
 	"name":"sucre",
         "components":"500g",
         "tags":["11", "22", "33"],
         "nutrients":[]
     },
     {
+	"_id":"33333",
 	"name":"lait",
         "components":"1L",
         "tags":["11", "22", "33"],
