a2af6a07e9c42165e4150b2cbe482122a4327f15
kaliazur <kaliazur@gmail.com>
2014-12-13 21:02:27 +0800

    Depot remit a neuf

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../server.js');
- 
-describe('GET /', function() {
-  it('respond with nocope', function(done) {
-    request(app).get('/').expect('Nocope', done);
-  });
-});
