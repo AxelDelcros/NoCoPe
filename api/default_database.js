b511bbe701493c1fb7baedfd36dd39bd3c6aca8c
kaliazur <kaliazur@gmail.com>
2015-01-04 16:05:14 +0800

    default database

--- api/default_database.js
+++ api/default_database.js
@@ -15,7 +15,20 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	"duration":"1H15",
-	"steps":[],
+	"steps":[{"title":"Preparation",
+		  "duration":"15",
+		  "content":"Prendre tout les ingredients et tout mettre dans un ramequi."
+		 },
+		 {
+		     "title":"Melange",
+		     "duration":"30",
+		     "content":"Battre tout jusqu'a ce que cela devienne bien jaune ..."
+		 },
+		 {
+		     "title":"Service",
+		     "duration":"45",
+		     "content":"Retourner le plat pour voir si sa tient, et servir dans les bols bien chaud."
+		 }],
 	"ingredients":["111111111111111111111111", "222222222222222222222222"],
 	"tools":["222222222222222222222222", "333333333333333333333333"],
 	"products":[],
@@ -38,7 +51,26 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	"duration":"2H10",
-	"steps":[],
+	"steps":[{"title":"Regarder les ingredients",
+		  "duration":"5",
+		  "content":"Regardez tout les ingredients pendant 5min pour etre bien sur qu'il soit bien la, on ne sait jamais !"
+		 },
+		 {"title":"Musique",
+		  "duration":"15",
+		  "content":"Tappez dans vos mains, les ingredients aiment bien la musique comme ca, cela les rassure et leur viande en est plus tendre !"
+		 },
+		 {"title":"Jeu",
+		  "duration":"30",
+		  "content":"Apres ce dur effort, aller vous detendre et jouez a votre jeu video prefere pendant une demi heure !"
+		 },
+		 {"title":"Melange",
+		  "duration":"25",
+		  "content":"Il faut tout melanger dans le grand saladier, bien fort avec le fouet !"
+		 },
+		 {"title":"Cuisson",
+		  "duration":"45",
+		  "content":"Cuire au four pendant 3 quard d heure ! Servez c est pret !!!"
+		 }],
 	"ingredients":["222222222222222222222222", "333333333333333333333333"],
 	"tools":["111111111111111111111111", "222222222222222222222222"],
 	"products":[],
@@ -61,7 +93,14 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	"duration":"2H42",
-	"steps":[],
+	"steps":[{"title":"On melange tout",
+		  "duration":"15",
+		  "content":"Mettre les patate avec la creme fouette."
+		 },
+		 {"title":"Service",
+		  "duration":"5",
+		  "content":"On sert tout, voila, bonne degustation !"
+		 }],
 	"ingredients":["111111111111111111111111", "333333333333333333333333"],
 	"tools":["111111111111111111111111", "333333333333333333333333"],
 	"products":[],
