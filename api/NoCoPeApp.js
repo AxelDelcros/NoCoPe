e2c2523521231b7be8357a118923eec78555e0a6
kaliazur <kaliazur@gmail.com>
2015-01-07 17:57:08 +0800

    Headers

--- api/NoCoPeApp.js
+++ api/NoCoPeApp.js
@@ -72,8 +72,8 @@ app.use(function(req,res,next){
 
     // Allow cross-domain requests
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    //res.header("Access-Control-Allow-Methods", "POST");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Methods", "POST");
     res.header("Access-Control-Request-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
