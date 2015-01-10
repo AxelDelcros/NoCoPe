72c4626b35407f34e6e4c5e8428af9590c2e9401
kaliazur <kaliazur@gmail.com>
2015-01-10 15:16:09 +0800

    Nocope.js

--- api/NoCoPeApp.js
+++ api/NoCoPeApp.js
@@ -74,7 +74,7 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET");
     res.header("Access-Control-Request-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "access_token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
